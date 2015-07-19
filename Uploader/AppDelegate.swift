2c597985d9f559a57fb248358d1983e7ad7d9b6a
Łukasz Adamczak <lukasz@czak.pl>
2015-07-19 13:47:23 +0200

    Detect new screenshots using NSMetadataQuery

--- Uploader/AppDelegate.swift
+++ Uploader/AppDelegate.swift
@@ -14,23 +14,35 @@ private let serverURLKey = "serverURL"
 class AppDelegate: NSObject, NSApplicationDelegate, StatusItemViewDelegate {
     var statusItemView: StatusItemView!
     var uploader: Uploader!
+    let query = NSMetadataQuery()
     
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         statusItemView = StatusItemView()
         statusItemView.delegate = self
         
+        // Register user defaults
         let userDefaults = NSUserDefaults.standardUserDefaults()
         userDefaults.registerDefaults([
             serverURLKey: "http://localhost:8080/upload/"
         ])
-        
+
         let url = userDefaults.stringForKey(serverURLKey)!
         uploader = Uploader(serverURL: url)
+        
+        // Metadata query for new screenshots
+        let notificationCenter = NSNotificationCenter.defaultCenter()
+        notificationCenter.addObserver(self, selector: "queryUpdated:", name: NSMetadataQueryDidUpdateNotification, object: query)
+        
+        query.predicate = NSPredicate(format: "kMDItemIsScreenCapture = 1")
+        query.sortDescriptors = [NSSortDescriptor(key: "kMDItemFSCreationDate", ascending: false)]
+        query.startQuery()
     }
     
-    // MARK: - StatusItemViewDelegate
+    func applicationWillTerminate(aNotification: NSNotification) {
+        query.stopQuery()
+    }
     
-    func statusItemView(view: StatusItemView, didReceiveFileURL fileURL: NSURL) {
+    func uploadFile(fileURL: NSURL) {
         uploader.uploadFile(fileURL, completionHandler: { result in
             switch result {
             case .Success(let link):
@@ -43,6 +55,31 @@ class AppDelegate: NSObject, NSApplicationDelegate, StatusItemViewDelegate {
             }
         })
     }
+    
+    // MARK: - StatusItemViewDelegate
+    
+    func statusItemView(view: StatusItemView, didReceiveFileURL fileURL: NSURL) {
+        uploadFile(fileURL)
+    }
 
+    // MARK: - NSMetadataQuery notification
+    
+    func queryUpdated(notification: NSNotification) {
+        println("Query updated: \(query.resultCount) screenshots found")
+        
+        if let item = query.resultAtIndex(0) as? NSMetadataItem {
+            if let path = item.valueForAttribute("kMDItemPath") as? String,
+                let fileURL = NSURL(fileURLWithPath: path) {
+                uploadFile(fileURL)
+            }
+            else {
+                println("Unable to parse URL from \(item.attributes)")
+            }
+        }
+        else {
+            println("No item in query")
+        }
+    }
+    
 }
 
