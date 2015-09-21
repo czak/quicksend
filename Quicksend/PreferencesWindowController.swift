1269232c6726000f8050fbbfa7c9a64f0d0f8a17
Łukasz Adamczak <lukasz@czak.pl>
2015-09-21 18:59:38 +0200

    Lista regionów AWS do dropdowna; poprawne bindingi i defaultsy

--- Quicksend/PreferencesWindowController.swift
+++ Quicksend/PreferencesWindowController.swift
@@ -8,10 +8,38 @@
 
 import Cocoa
 
-class PreferencesWindowController: NSWindowController {
+private let awsAccessKeyIdKey = "awsAccessKeyId"
+private let awsSecretAccessKeyKey = "awsSecretAccessKey"
+private let awsBucketNameKey = "awsBucketName"
+private let awsRegionKey = "awsRegion"
 
+class PreferencesWindowController: NSWindowController, NSWindowDelegate {
+
+    var regions = [
+        [ "title": "US Standard (N. Virginia)", "key": "us-east-1" ],
+        [ "title": "US West (Oregon)",          "key": "us-west-2" ],
+        [ "title": "US West (N. California)",   "key": "us-west-1" ],
+        [ "title": "EU (Ireland)",              "key": "eu-west-1" ],
+        [ "title": "EU (Frankfurt)",            "key": "eu-central-1" ],
+        [ "title": "Asia Pacific (Singapore)",  "key": "ap-southeast-1" ],
+        [ "title": "Asia Pacific (Sydney)",     "key": "ap-southeast-2" ],
+        [ "title": "Asia Pacific (Tokyo)",      "key": "ap-northeast-1" ],
+        [ "title": "South America (Sao Paulo)", "key": "sa-east-1" ]
+    ]
+    
     override var windowNibName: String {
         return "PreferencesWindowController"
+    }
+    
+    override func windowDidLoad() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.registerDefaults([
+            awsRegionKey: "us-east-1"
+        ])
+    }
+    
+    func windowWillClose(notification: NSNotification) {
+        window!.endEditingFor(nil)
     }
     
 }
