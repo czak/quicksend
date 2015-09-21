083cbc5335ce9fe5130edc63bbcc9a5430e0a4fc
Łukasz Adamczak <lukasz@czak.pl>
2015-09-21 19:23:44 +0200

    Uploader bierze dane z user defaults

--- Quicksend/Uploader.swift
+++ Quicksend/Uploader.swift
@@ -9,17 +9,29 @@
 import Foundation
 import Alamofire
 
-let AWSAccessKeyID = "AKIAJFGBQKHT7H53DOYQ"
-let AWSSecretAccessKey = "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"
-let AWSBucketName = "czak-screenshots"
-let AWSRegion = "eu-west-1"
-
 class Uploader {
     enum UploadStatus {
         case Success(String)
         case Failure(String)
     }
     
+    // Zmienne konfiguracyjne AWSa
+    var awsAccessKeyId: String! {
+        return NSUserDefaults.standardUserDefaults().stringForKey("awsAccessKeyId")
+    }
+
+    var awsSecretAccessKey: String! {
+        return NSUserDefaults.standardUserDefaults().stringForKey("awsSecretAccessKey")
+    }
+    
+    var awsBucketName: String! {
+        return NSUserDefaults.standardUserDefaults().stringForKey("awsBucketName")
+    }
+    
+    var awsRegion: String! {
+        return NSUserDefaults.standardUserDefaults().stringForKey("awsRegion")
+    }
+
     func mimetypeForFile(url: NSURL) -> String {
         let ext = url.pathExtension
         let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, ext!, nil)!.takeRetainedValue()
@@ -34,6 +46,11 @@ class Uploader {
     }
     
     func uploadFile(fileURL: NSURL, completionHandler: (UploadStatus) -> Void) {
+        guard (awsAccessKeyId != nil && awsSecretAccessKey != nil && awsBucketName != nil && awsRegion != nil) else {
+            completionHandler(.Failure("Not configured correctly!"))
+            return
+        }
+        
         // 1. Canonical request
         let fileName = fileURL.lastPathComponent!
         let fileData = NSData(contentsOfURL: fileURL)!
@@ -41,7 +58,7 @@ class Uploader {
         let date = NSDate()
         let mimetype = mimetypeForFile(fileURL)
         
-        let creq = "PUT\n/\(fileName)\n\ncontent-type:\(mimetype)\nhost:\(AWSBucketName).s3.amazonaws.com\nx-amz-acl:public-read\nx-amz-content-sha256:\(fileHash)\nx-amz-date:\(date.iso8601timestamp())\n\ncontent-type;host;x-amz-acl;x-amz-content-sha256;x-amz-date\n\(fileHash)"
+        let creq = "PUT\n/\(fileName)\n\ncontent-type:\(mimetype)\nhost:\(awsBucketName).s3.amazonaws.com\nx-amz-acl:public-read\nx-amz-content-sha256:\(fileHash)\nx-amz-date:\(date.iso8601timestamp())\n\ncontent-type;host;x-amz-acl;x-amz-content-sha256;x-amz-date\n\(fileHash)"
         
         debugPrint("--- CREQ ---")
         print(creq)
@@ -50,7 +67,7 @@ class Uploader {
         // 2. String to sign
         let creqHash = sha256_hexdigest(creq.dataUsingEncoding(NSUTF8StringEncoding)!)
         
-        let sts = "AWS4-HMAC-SHA256\n\(date.iso8601timestamp())\n\(date.iso8601datestamp())/\(AWSRegion)/s3/aws4_request\n\(creqHash)"
+        let sts = "AWS4-HMAC-SHA256\n\(date.iso8601timestamp())\n\(date.iso8601datestamp())/\(awsRegion)/s3/aws4_request\n\(creqHash)"
         
         print("--- STS ---")
         print(sts)
@@ -58,9 +75,9 @@ class Uploader {
         
         // 3. Signing key
         
-        let signingKey = awsSigningKey(key: AWSSecretAccessKey,
+        let signingKey = awsSigningKey(key: awsSecretAccessKey,
             date: date.iso8601datestamp(),
-            region: AWSRegion,
+            region: awsRegion,
             service: "s3")
         
         print(signingKey)
@@ -74,14 +91,14 @@ class Uploader {
         // 5. Wykonanie requestu
         
         let headers = [
-            "Authorization": "AWS4-HMAC-SHA256 Credential=\(AWSAccessKeyID)/\(date.iso8601datestamp())/\(AWSRegion)/s3/aws4_request, SignedHeaders=content-type;host;x-amz-acl;x-amz-content-sha256;x-amz-date, Signature=\(signature)",
+            "Authorization": "AWS4-HMAC-SHA256 Credential=\(awsAccessKeyId)/\(date.iso8601datestamp())/\(awsRegion)/s3/aws4_request, SignedHeaders=content-type;host;x-amz-acl;x-amz-content-sha256;x-amz-date, Signature=\(signature)",
             "Content-Type": mimetype,
             "x-amz-acl": "public-read",
             "x-amz-content-sha256": fileHash,
             "x-amz-date": date.iso8601timestamp()
         ]
         
-        let request = Alamofire.upload(.PUT, "https://\(AWSBucketName).s3.amazonaws.com/\(fileName)", headers: headers, file: fileURL)
+        let request = Alamofire.upload(.PUT, "https://\(awsBucketName).s3.amazonaws.com/\(fileName)", headers: headers, file: fileURL)
         request.response { request, response, data, error in
             var status: UploadStatus?
 
